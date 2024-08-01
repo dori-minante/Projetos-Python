305a3161f588a272f70b88a252b6f37268db24b1
Dori Minante <dorim.dev@gmail.com>
2024-08-01 20:12:24 -0300

    exercicios

---
+++ exercicios/Exercicios/main.py
@@ -0,0 +1,32 @@
+#Escreva as seguintes expressões em linguagem python:
+#a) O somatório dos 5 primeiros números inteiros e positivos
+
+print(1 + 2 + 3 + 4 + 5)
+
+#b) A média entre 23,19 e 31
+
+print((23 + 19 + 31) / 3)
+
+#c) O número de vezes que 73 cabe em 403
+# / - divisão com casas decimais
+# // - divisão somente com a parte inteira
+
+print(403 // 73)
+
+#d) A sobra quando 403 é dividido por 73
+# % - módulo/resto da divisão
+print(403 % 73)
+
+#e) 2 elevado à 10ª potência
+
+print(2 ** 10)
+
+#f) O valor absoluto da diferença entre 54 e 57
+# abs - valor absoluto
+
+print(abs(54- 57))
+
+#g) O menor valor entre 34, 29 e 31
+#min - menor valor do que estiver em parênteses
+
+print(min(34, 29, 31))
