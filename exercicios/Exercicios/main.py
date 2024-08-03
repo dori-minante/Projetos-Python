0e9235d1ca53e973bf13ec813b628a4f29d1f7c2
Dori Minante <dorim.dev@gmail.com>
2024-08-03 19:58:10 -0300

    mudanças nos exercicios

--- exercicios/Exercicios/main.py
+++ exercicios/Exercicios/main.py
@@ -24,9 +24,52 @@ print(2 ** 10)
 #f) O valor absoluto da diferença entre 54 e 57
 # abs - valor absoluto
 
-print(abs(54- 57))
+print(abs(54 - 57))
 
 #g) O menor valor entre 34, 29 e 31
 #min - menor valor do que estiver em parênteses
 
 print(min(34, 29, 31))
+
+# Exercicios de atribuição
+# a) Atribuir o valor inteiro 3 a variável a
+# b) Atribuir o valor 4 a variável b
+# c) Atribuir a variavel c o valor da expressão a*a + b*b
+
+a = 3
+b = 4
+c = a * a + b * b
+
+print(c)
+
+# Strings
+# Execute as seguintes atribuições:
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+# Agora, utilizando operadores + e*, crie saídas a seguir:
+# a) 'ant bat cod'
+# b) 'ant ant ant ant ant ant ant ant ant ant'
+# c) 'ant bat bat cod cod cod'
+# d) 'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
+# e) 'batbatcod batbatcod batbatcod batbatcod'
+
+# a)
+res = s1 + ' ' + s2 + ' ' + s3
+print(res)
+
+# b)
+res = 10 * (s1 + ' ')
+print(res)
+
+# c)
+res = (s1 + ' ') + 2 * (s2 + ' ') + 3 * (s3 + ' ')
+print(res)
+
+# d)
+res = 7 * (s1 + ' ' + s2 + ' ')
+print(res)
+
+# e)
+res = 4 * (s1 + s2 + s3 + ' ')
+print(res)
