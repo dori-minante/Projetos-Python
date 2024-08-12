dfc8f223ef4c840b0ea598aa7dd1ec5438a68ebb
Dori Minante <dorim.dev@gmail.com>
2024-08-12 15:52:40 -0300

    condicionais

---
+++ exercicio1/ex1/condicionais1.py
@@ -0,0 +1,11 @@
+# Verificar se o primeiro valor é maior do que o segundo.
+# E outro para verificar se o segundo é maior do que o primeiro.
+
+#Lê dois valores inteiros e compara ambos
+
+x = int(input('Digite um valor inteiro: '))
+y = int(input('Digite um segundo valor inteiro: '))
+if (x > y):
+ print('O primeiro valor é maior que o segundo!')
+if (x < y):
+ print('O segundo valor é maior que o segundo!')
