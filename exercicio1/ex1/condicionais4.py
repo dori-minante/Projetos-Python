c2b8314dd6be43e4cded20a33e747d865864c94e
Dori Minante <dorim.dev@gmail.com>
2024-08-12 23:17:51 -0300

    exercicios

---
+++ exercicio1/ex1/condicionais4.py
@@ -0,0 +1,27 @@
+# Escreva um algoritmo em Python em que o usuário escolhe se ele quer comprar maçãs, laranjas ou bananas. Deverá ser
+# apresentado na tela um menu com a opção 1 para maçã, 2 para laranja e 3 para banana.
+# Após escolhida a fruta, deve-se digitar quantas unidades se quer comprar.
+# O algoritmo deve calcular o preço total a pagar do produto escolhido e mostrá-lo na tela.
+# Considere que uma maçã custa R$ 2,30, uma laranja R$ 3,60 e uma banana 1,85
+
+print('Escolha o que deseja comprar:')
+print('1 - Maça')
+print('2 - Laranja')
+print('3 - Banana')
+
+produto = int(input('Qual sua escolha? '))
+qtd = int(input('Quantas unidades? '))
+
+if (produto == 1):
+    pagar = qtd * 2.3
+    print(f'Você comprou {qtd} maças. Total à pagar: R$ {pagar}')
+else:
+    if (produto == 2):
+        pagar = qtd * 3.6
+        print(f'Você comprou {qtd} laranjas. Total a pagar: R$ {pagar}')
+    else:
+        if (produto == 3):
+            pagar = qtd * 1.85
+            print(f'Você comprou {qtd} bananas. Total a pagar: R$ {pagar}')
+        else:
+            print('Produto inexistente!')
---
+++ exercicio1/ex1/exercicio6.py
@@ -0,0 +1,9 @@
+# Faça um algoritmo que receba três valores, representando os lados de um triângulo fornecidos pelo usuário.
+# Verifique se os valores formam um triângulo e classifique como:
+
+# a) Equilátero (três lados iguais);
+# b) Isósceles (dois lados iguais);
+# c) Escaleno (três lados diferentes).
+
+# Lembre-se de que, para formar um triângulo, nenhum dos lados pode ser igual a zero
+# e um lado não pode ser maior do que a soma dos outros dois.
