d4427d341da9a6fe1b109cdf545bba811d77afae
Dori Minante <dorim.dev@gmail.com>
2024-08-17 15:17:35 -0300

    exercicio

---
+++ projeto2/pythonProject/exercicio1.py
@@ -0,0 +1,26 @@
+#  Faça um algoritmo que receba três
+# valores, representando os lados de um triângulo fornecidos pelo usuário.
+# Verifique se os valores formam um triângulo e classifique como:
+# a) Equilátero (três lados iguais);
+# b) Isósceles (dois lados iguais);
+# c) Escaleno (três lados diferentes).
+# Lembre-se de que, para formar um triângulo, nenhum dos lados pode ser
+# igual a zero e um lado não pode ser maior do que a soma dos outros dois.
+
+A = int(input('Digite o 1º lado do triângulo: '))
+B = int(input('Digite o 2º lado do triângulo: '))
+C = int(input('Digite o 3º lado do triângulo: '))
+
+if ((A > 0 and B > 0 and C > 0) and (A + B > C and A + C > B and B + C > A)):
+    # Se chegar até aqui, é porque o triângulo é válido!
+
+    if (A != B and A != C and B != C):
+        print('Triângulo escaleno!')
+    else:
+        if (A == B and B == C):
+            print('Triângulo equilátero!')
+        else:
+            print('Triângulo isósceles!')
+else:
+    print('Ao menos um dos valores indicados não servem para formar um triângulo.')
+
