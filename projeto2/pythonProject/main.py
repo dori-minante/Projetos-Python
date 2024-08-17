1b2fb3540273dd0af99556350fa4c4ab19b3b4de
Dori Minante <dorim.dev@gmail.com>
2024-08-17 14:47:10 -0300

    exercicio

---
+++ projeto2/pythonProject/main.py
@@ -0,0 +1,20 @@
+# a) O somatório de 2 com 2 é menor que 4
+
+if (2 + 2 < 4):
+    print("Verdadeiro!")
+
+# b) O valor 7 // 3 é igual a 1 + 1
+
+if(7 // 3 == 1 + 1):
+    print("Verdadeiro!")
+
+# c) A soma de 3 elevado ao quadrado com 4 elevado ao quadrado é igual a 25
+
+if(3**2 + 4**2 == 25):
+    print("Verdadeiro!")
+
+# d) A soma de 2,4 e 6 é maior que 12
+
+if (2 + 4 + 6 > 12):
+    print("Verdadeiro!")
+
