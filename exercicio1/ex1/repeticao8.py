bd7bffc89cb30c3801935b9f84088c0a57d2253c
Dori Minante <dorim.dev@gmail.com>
2024-08-24 15:43:31 -0300

    ex

---
+++ exercicio1/ex1/repeticao8.py
@@ -0,0 +1,24 @@
+# Realize a sequência de print com for e while:
+
+# a) inteiros de 3 até 12, com 12 incluso
+
+i = 3
+
+while (i < 13):
+    print(i)
+    i += 1
+
+for i in range(3, 13, 1):
+    print(i)
+
+# b) Inteiros de 0 até 9, excluindo o 9, com passo de 2
+
+for i in range(0, 9, 2):
+    print(i)
+
+i = 0
+
+while (i <=8):
+    print(i)
+    i += 2
+
