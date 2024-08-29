6dd75754ea8bfec1d4b3db85cca0b7fbb10c1922
Dori Minante <dorim.dev@gmail.com>
2024-08-29 13:58:17 -0300

    funcao2

--- exercicio1/ex1/funcao2.py
+++ exercicio1/ex1/funcao2.py
@@ -11,3 +11,18 @@
 # Variáveis globais pertencem a um escopo global e são variáveis criadas dentro do programa principal.
 # Uma variável global existem também em todas as funções invocadas ao longo do programa
 
+# Escreva uma função para validar uma string. Essa função recebe como parâmetro a string, o número mínimo e máximo de caracteres.
+# Retorne verdadeiro se o tamanho da string estiver entre os valores de mínimo e máximo,
+# e falso, caso contrário
+
+def valida_string(pergunta, min, max):
+    s1 = input(pergunta)
+    tam = len(s1)
+    while ((tam < min) or (tam > max)):
+        s1 = input(pergunta)
+        tam = len(s1)
+    return s1
+
+#programa principal
+x = valida_string('Digite uma string: ', 10, 30)
+print('Você digitou a string: {}. \n Dado válido. Encerrando o programa...'.format(x))
