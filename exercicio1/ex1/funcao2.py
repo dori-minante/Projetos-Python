608ec3b23a5f991900e209132e38d1b84392c45f
Dori Minante <dorim.dev@gmail.com>
2024-08-29 13:38:50 -0300

    funcao2

---
+++ exercicio1/ex1/funcao2.py
@@ -0,0 +1,13 @@
+# Escopo de variáveis
+# Um escopo é a propriedade que determina onde uma variável pode ser utilizada dentro de um programa
+
+#Escopo local
+# Criado sempre que uma função é chamada
+# Variáveis criadas, seja no campo de um parâmetro ou dentro do corpo da função,
+# fazem parte do escopo local daquela função e são chamadas de variáveis locais.
+# Essas variáveis só existem dentro daquela própria função
+
+#Escopo global
+# Variáveis globais pertencem a um escopo global e são variáveis criadas dentro do programa principal.
+# Uma variável global existem também em todas as funções invocadas ao longo do programa
+
